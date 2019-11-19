2584af90328336fdf79cca04fcf15181ea2f6e45
Alma Mamaradlo <almarkmedina@gmail.com>
2019-11-19 16:46:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Tweet from './Tweet';
 
 function App(){
   return (
-    <div>
-    <h1>Hello there!! </h1>
+    <div className="app">
+      <Tweet name="Al" message="Hello there!"/>
+      <Tweet name="Mark" message="What's up?"/>
+      <Tweet name="Mason" message="How you doin?"/>
+
     </div>
+
   );
 }
 
